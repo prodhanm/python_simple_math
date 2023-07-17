35a138489208ed3ff3dd84052fdc4bf369f381c4
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-17 10:20:11 -0400

    coding a list comprehension and a new list in a function.

--- math_1.py
+++ math_1.py
@@ -7,7 +7,17 @@ def multi():
 print(multi())
 
 def multi_list():
-    prod_li = [num*2 if num%2 == 0 else num//2 for num in range(1,24)]
+    prod_li = [num*2 if num%2 == 0 else num//2 
+               for num in range(1,24)]
     return prod_li
 
 print(f"products = {multi_list()}")
+
+def app_mult():
+    num_li = [num for num in range(1,24)]
+    double_nums = []
+    for nums in range(len(num_li)):
+        double_nums.append(num_li[nums]*2)
+    return double_nums
+
+print(f"double multiples = {app_mult()}")
