b182280bc54bf2cbb6d97c071ec78354b59ba31e
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-17 11:49:10 -0400

    added more functions and modified .py file.

--- math_1.py
+++ math_1.py
@@ -13,11 +13,53 @@ def multi_list():
 
 print(f"products = {multi_list()}")
 
-def app_mult():
+def li_comp():
     num_li = [num for num in range(1,24)]
+    return num_li
+
+def app_mult():
     double_nums = []
-    for nums in range(len(num_li)):
-        double_nums.append(num_li[nums]*2)
+    for nums in range(len(li_comp())):
+        double_nums.append(li_comp()[nums]*2)
     return double_nums
 
 print(f"double multiples = {app_mult()}")
+
+def square():
+    square_li = [li_comp()[num]**2 for num in range(len(li_comp()))]
+    return square_li
+
+print(f"sqaured numbers = {square()}")
+
+def tot():
+    total = sum(li_comp())
+    return total
+
+print(f"The total of the list from li_comp() is {tot()}")
+
+def average():
+    
+    avg = tot()/len(li_comp())
+    return avg
+
+print(f"The average of the list comprehension from li_comp() is {average()}")
+
+def subtr():
+    diff = 0
+    for num in range(len(li_comp())):
+        diff -= li_comp()[num]
+    return diff
+
+print(f"The differnce of all of the numbers from the list in li_comp() is {subtr()}")
+
+def straight():
+    floor_avg = tot()//len(li_comp())
+    return floor_avg
+
+print(f"The floor average of the list from li_comp() is {straight()}")
+
+def diff_floor():
+    diff_avg = subtr()//len(li_comp())
+    return diff_avg
+
+print(f"The floor average of the values subtracted in li_comp() is {diff_floor()}")
