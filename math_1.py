97fa71266f43276b13a1d3db898b3f1b44dccd3f
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-17 18:30:40 -0400

    added error handling to the code.

--- math_1.py
+++ math_1.py
@@ -1,65 +1,94 @@
 def multi():
-    product = 1
-    for num in range(1,24):
-        product *= num
-    return product
+    try:
+        product = 1
+        for num in range(1,24):
+            product *= num
+        return product
+    except ValueError as err:
+        print(err)
 
 print(multi())
 
 def multi_list():
-    prod_li = [num*2 if num%2 == 0 else num//2 
-               for num in range(1,24)]
-    return prod_li
+    try:
+        prod_li = [num*2 if num%2 == 0 else num//2 
+                   for num in range(1,24)]
+        return prod_li
+    except ValueError as err:
+        print(err)
 
 print(f"products = {multi_list()}")
 
 def li_comp():
-    num_li = [num for num in range(1,24)]
-    return num_li
+    try:
+        num_li = [num for num in range(1,24)]
+        return num_li
+    except ValueError as err:
+        print(err)
 
 def app_mult():
-    double_nums = []
-    for nums in range(len(li_comp())):
-        double_nums.append(li_comp()[nums]*2)
-    return double_nums
+    try:
+        double_nums = []
+        for nums in range(len(li_comp())):
+            double_nums.append(li_comp()[nums]*2)
+        return double_nums
+    except ValueError as err:
+        print(err)
 
 print(f"double multiples = {app_mult()}")
 
 def square():
-    square_li = [li_comp()[num]**2 for num in range(len(li_comp()))]
-    return square_li
+    try:
+        square_li = [li_comp()[num]**2 for num in range(len(li_comp()))]
+        return square_li
+    except ValueError as err:
+        print(err)
 
 print(f"sqaured numbers = {square()}")
 
 def tot():
-    total = sum(li_comp())
-    return total
+    try:
+        total = sum(li_comp())
+        return total
+    except ValueError as err:
+        print(err)
 
 print(f"The total of the list from li_comp() is {tot()}")
 
 def average():
-    
-    avg = tot()/len(li_comp())
-    return avg
+    try:
+        avg = tot()/len(li_comp())
+        return avg
+    except ValueError as err:
+        print(err)
 
 print(f"The average of the list comprehension from li_comp() is {average()}")
 
 def subtr():
-    diff = 0
-    for num in range(len(li_comp())):
-        diff -= li_comp()[num]
-    return diff
+    try:
+        diff = 0
+        for num in range(len(li_comp())):
+            diff -= li_comp()[num]
+        return diff
+    except ValueError as err:
+        print(err)
 
 print(f"The differnce of all of the numbers from the list in li_comp() is {subtr()}")
 
 def straight():
-    floor_avg = tot()//len(li_comp())
-    return floor_avg
+    try:
+        floor_avg = tot()//len(li_comp())
+        return floor_avg
+    except ValueError as err:
+        print(err)
 
 print(f"The floor average of the list from li_comp() is {straight()}")
 
 def diff_floor():
-    diff_avg = subtr()//len(li_comp())
-    return diff_avg
-
+    try:
+        diff_avg = subtr()//len(li_comp())
+        return diff_avg
+    except ValueError as err:
+        print(err)
+        
 print(f"The floor average of the values subtracted in li_comp() is {diff_floor()}")
